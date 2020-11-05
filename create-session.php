2d07cff4a44fd79ed9b2c9c1737a423e139cc180
Antoine Valet <Antoine@imac-de-antoine.home>
2020-11-05 15:36:13 +0100

    update

--- create-session.php
+++ create-session.php
@@ -11,11 +11,11 @@ $checkout_session = \Stripe\Checkout\Session::create([
   'payment_method_types' => ['card'],
   'line_items' => [[
     'price_data' => [
-      'currency' => 'eur',
-      'unit_amount' => 6000,
+      'currency' => 'usd',
+      'unit_amount' => 2000,
       'product_data' => [
-        'name' => 'Prépaiement remorquage',
-        'images' => ["/assets/remorque.jpeg"],
+        'name' => 'Stubborn Attachments',
+        'images' => ["https://i.imgur.com/EHyR2nP.png"],
       ],
     ],
     'quantity' => 1,
